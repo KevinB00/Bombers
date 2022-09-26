cbc63b1ecadd9cd8583385e66dd35635ffdf71eb
KevinB00 <kevbueeva@alu.edu.gva.es>
2022-09-26 22:06:21 +0200

    aplicando metodo seleccionar()

--- Bombers/src/main/java/pruebasnb/bombers/BomberDAO.java
+++ Bombers/src/main/java/pruebasnb/bombers/BomberDAO.java
@@ -8,10 +8,39 @@ import java.util.logging.Logger;
 
 public class BomberDAO {
 
-    private static final String SQL_SELECT = "SELECT CodBomber, Nom, Adreca, CodParc, CodCarrec, CodEquipo";
-    private static final String SQL_INSERT = "INSERT INTO CodBomber (CodBomber, Nom, Adreca, CodParc, CodCarrec, CodEquipo) VALUES (?,?,?,?,?,?)";
+    private static final String SQL_SELECT = "SELECT CodBomber, Nom, Adreca, CodParc, CodCarrec, CodEquip";
+    private static final String SQL_INSERT = "INSERT INTO CodBomber (CodBomber, Nom, Adreca, CodParc, CodCarrec, CodEquip) VALUES (?,?,?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE bomber SET CodBomber=?, Nom=?, Adreca=?, CodParc=?, CodCarrec=?, CodEquip=? WHERE CodBomber=?";
+    private static final String SQL_DELETE = "DELETE FROM bomber WHERE CodBomber=?";
+
+    public List<Bomber> seleccionar() throws SQLException {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Bomber bomber = null;
+        List<Bomber> bombers = new ArrayList<>();
+
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int CodBomber = rs.getInt("CodBomber");
+                String Nom = rs.getString("Nom");
+                String Adreca = rs.getString("Adreca");
+                int CodParc = rs.getInt("CodParc");
+                int CodCarrec = rs.getInt("CodCarrec");
+                int CodEquip = rs.getInt("CodEquip");
+
+                bomber = new Bomber(CodBomber, Nom, Adreca, CodParc, CodCarrec, CodEquip);
+                bombers.add(bomber);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
 
-
-
+        return bombers;
+    }
 }
